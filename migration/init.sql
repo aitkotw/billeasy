1d2c34f68c9c95983ccc7ebb7e59e12156f78130
abhishek <abhishek.mishra@p2epl.com>
2022-09-20 02:19:18 +0530

    final update

--- migration/init.sql
+++ migration/init.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.5 (Debian 14.5-1.pgdg110+1)
 -- Dumped by pg_dump version 14.5
 
--- Started on 2022-09-19 17:25:31 UTC
+-- Started on 2022-09-19 20:45:05 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -28,12 +28,11 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.department (
-    id character varying NOT NULL,
-    name character varying NOT NULL,
     description character varying,
     createdat timestamp with time zone,
     location character varying,
-    totalemployees integer
+    totalemployees integer DEFAULT 0,
+    name character varying NOT NULL
 );
 
 
@@ -51,43 +50,25 @@ CREATE TABLE public.employee (
     email character varying NOT NULL,
     address character varying,
     department character varying NOT NULL,
-    "createdAt" timestamp with time zone NOT NULL
+    createdat timestamp with time zone NOT NULL
 );
 
 
 ALTER TABLE public.employee OWNER TO postgres;
 
---
--- TOC entry 3314 (class 0 OID 16392)
--- Dependencies: 210
--- Data for Name: department; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.department (id, name, description, createdat, location, totalemployees) FROM stdin;
-\.
 
 
 --
--- TOC entry 3313 (class 0 OID 16385)
--- Dependencies: 209
--- Data for Name: employee; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.employee (id, name, phone, email, address, department, "createdAt") FROM stdin;
-\.
-
-
---
--- TOC entry 3173 (class 2606 OID 16398)
--- Name: department department_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 3174 (class 2606 OID 16401)
+-- Name: department dep_pKey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.department
-    ADD CONSTRAINT department_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT "dep_pKey" PRIMARY KEY (name);
 
 
 --
--- TOC entry 3171 (class 2606 OID 16391)
+-- TOC entry 3172 (class 2606 OID 16391)
 -- Name: employee employee_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -95,7 +76,7 @@ ALTER TABLE ONLY public.employee
     ADD CONSTRAINT employee_pkey PRIMARY KEY (id);
 
 
--- Completed on 2022-09-19 17:25:31 UTC
+-- Completed on 2022-09-19 20:45:05 UTC
 
 --
 -- PostgreSQL database dump complete
